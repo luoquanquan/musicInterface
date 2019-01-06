85b84f5fa96a78b761a4382d97fb5095c808e63e
luoquanquan <luoquanquan@baidu.com>
2019-01-06 15:53:48 +0800

    接口返回数据添加声明

--- src/middlewares/responseFormatter.js
+++ src/middlewares/responseFormatter.js
@@ -1,3 +1,10 @@
+/*
+ * @file: 接口输出信息格式化
+ * @Author: luoquanquan
+ * @Date: 2019-01-04 10:20:10
+ * @LastEditors: luoquanquan
+ * @LastEditTime: 2019-01-06 15:51:52
+ */
 /**
  * 在app.use(router)之前调用
  */
@@ -7,6 +14,9 @@ module.exports = async (ctx, next) => {
   if (typeof ctx.body === 'object') {
     if (!ctx.body.errno) {
       ctx.body = {
+        announce: '本接口所有数据均来自 QQ 音乐, 仅供学习交流之用,' +
+                  '请不要用于商业用途. 如果喜欢请下载 QQ 音乐 APP 畅听.' +
+                  '如有侵权请联系微信(QQ): 1363693666, 我会第一时间删除~',
         errno: 0,
         msg: 'success',
         data: ctx.body
